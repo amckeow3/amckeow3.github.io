14041b2887fa0385e5e04c88192c200cb5be901d
Adrianna McKeown <amckeow3@uncc.edu>
2022-04-11 09:59:14 -0400

    collapsable menu works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
-import { HeroComponent } from './components/hero/hero.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { WorkComponent } from './components/work/work.component';
 import { ContactComponent } from './components/contact/contact.component';
@@ -15,16 +15,16 @@ import { ContactComponent } from './components/contact/contact.component';
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent,
     AboutComponent,
-    HeroComponent,
     SkillsComponent,
     WorkComponent,
     ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgImageSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/work/work.component.ts
+++ src/app/components/work/work.component.ts
@@ -12,4 +12,68 @@ export class WorkComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  forumsAppImages: Array<object> = [{
+    image: 'assets/images/projects/firebase-forums/forums-homepage.png',
+    thumbImage: 'assets/images/projects/firebase-forums/forums-homepage.png',
+    title: 'forums home',
+    order: 1
+  }, {
+    image: 'assets/images/projects/firebase-forums/view-forum.png', 
+    thumbImage: 'assets/images/projects/firebase-forums/view-forum.png', 
+    title: 'view forum comments', 
+    order: 2
+  }, {
+    image: 'assets/images/projects/firebase-forums/new-comment-error.png', 
+    thumbImage: 'assets/images/projects/firebase-forums/new-comment-error.png', 
+    title: 'new comment error if submitted text is null', 
+    order: 3
+  }, {
+    image: 'assets/images/projects/firebase-forums/create-forum.png',
+    thumbImage: 'assets/images/projects/firebase-forums/create-forum.png',
+    title: 'create forum',
+    order: 4
+  }, {
+    image: 'assets/images/projects/firebase-forums/new-forum-error.png',
+    thumbImage: 'assets/images/projects/firebase-forums/new-forum-error.png',
+    title: 'create forum error if any fields are null',
+    order: 5
+  },{
+    image: 'assets/images/projects/firebase-forums/login.png',
+    thumbImage: 'assets/images/projects/firebase-forums/login.png',
+    title: 'login page',
+    order: 6
+  }, {
+    image: 'assets/images/projects/firebase-forums/login-error.png',
+    thumbImage: 'assets/images/projects/firebase-forums/login-error.png',
+    title: 'invalid username error',
+    order: 7
+  }, {
+    image: 'assets/images/projects/firebase-forums/login-password-error.png',
+    thumbImage: 'assets/images/projects/firebase-forums/login-password-error.png',
+    title: 'invalid password error',
+    order: 8
+  }, {
+    image: 'assets/images/projects/firebase-forums/registration.png',
+    thumbImage: 'assets/images/projects/firebase-forums/registration.png',
+    title: 'registration page',
+    order: 9
+  }, {
+    image: 'assets/images/projects/firebase-forums/registration-error.png',
+    thumbImage: 'assets/images/projects/firebase-forums/registration-error.png',
+    title: 'email already in use registration error ',
+    order: 10
+  }];
+
+  discountCalcImages: Array<object> = [{  
+    image: 'assets/images/discount-calculator/discount-initial.png',
+    thumbImage: 'assets/images/discount-calculator/discount-initial.png',
+    title: 'Initial Screen',
+    order: 1
+  }, {
+    image: 'assets/images/discount-calculator/discount-calculation.png', 
+    thumbImage: 'assets/images/discount-calculator/discount-calculation.png',
+    title: 'Discount Calculation', 
+    order: 2
+  }];
+
 }
